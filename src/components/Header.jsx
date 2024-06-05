0796c953e54241fad9a4b9c50cdf9de39f5ec8af
shining-jung <tjddnjz51w@gmail.com>
2024-06-05 10:00:38 +0900

    Header -- 구조 수정 Common.js - 버튼 공통 스타일 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,59 @@
 import { Link } from "react-router-dom";
+import style from "../css/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={style.header}>
       <h1>
         <Link to="/">
           <img src={`${process.env.PUBLIC_URL}/img/common/logo.svg`} alt="logo" />
         </Link>
       </h1>
-      <nav>
-        <Link to="/job-list">잡리스트</Link>
-        <Link to="/recruitment-management">구인관리</Link>
-        <Link to="/job-seeker-management">구직관리</Link>
-      </nav>
-      <div>
-        <Link to="/">아이콘</Link>
-        <Link to="/">
-          <button>회원가입</button>
-        </Link>
+
+      <div className={style.gnb}>
+        <nav>
+          <Link to="/">일자리찾기</Link>
+          <Link to="/">지원목록</Link>
+          <Link to="/">공고관리</Link>
+          <Link to="/">구인글 작성</Link>
+        </nav>
+
+        <div className={style.loginDiv}>
+          <button>
+            <i className="fa-regular fa-bell"></i>
+            <span></span>
+          </button>
+          <button>
+            <i className="fa-regular fa-comment-dots"></i>
+            <span></span>
+          </button>
+          <div className={style.thumb}>
+            <img src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`} alt="logo" />
+          </div>
+        </div>
+
+        <div className={style.logoutDiv}>
+          <Link to="/login" className={style.goLogin}>
+            <i className="fa-regular fa-user"></i>
+          </Link>
+          <Link to="/" className={`${style.goJoin} btn primary yellow`}>
+            회원가입
+          </Link>
+        </div>
+      </div>
+
+      <div className={style.mypage}>
+        <div className={style.myprofile}>
+          <h2>이름</h2>
+          <p>abc@asdd.com</p>
+        </div>
+        <span>빠른메뉴</span>
+        <Link to="/">내가 지원한 일자리</Link>
+        <Link to="/">구인글 작성</Link>
+        <Link to="/">구인글 관리</Link>
+        <Link to="/">프로필 수정</Link>
+        <Link to="/">회원정보 수정</Link>
+        <Link to="/">로그아웃</Link>
       </div>
     </header>
   );
