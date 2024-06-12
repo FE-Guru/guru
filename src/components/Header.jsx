5e4841c1839e01ff0c2faf6ecd391388f2a2721c
shining-jung <tjddnjz51w@gmail.com>
2024-06-12 10:05:34 +0900

    Header 부분 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,35 +16,29 @@ const Header = () => {
     <header className={style.header}>
       <h1>
         <Link to="/">
-          <img
-            src={`${process.env.PUBLIC_URL}/img/common/logo.svg`}
-            alt="logo"
-          />
+          <img src={`${process.env.PUBLIC_URL}/img/common/logo.svg`} alt="logo" />
         </Link>
       </h1>
 
       <div className={style.gnb}>
         <nav>
           <Link to="/findjob">일자리찾기</Link>
-          <Link to="/">지원목록</Link>
-          <Link to="/">공고관리</Link>
-          <Link to="/">구인글 작성</Link>
+          <Link to="/applied-list">지원목록</Link>
+          <Link to="/job-offer">공고관리</Link>
+          <Link to="/job-wirt">구인글 작성</Link>
         </nav>
 
         <div className={style.loginDiv}>
-          <button>
+          {/* <button>
             <i className="fa-regular fa-bell"></i>
             <span></span>
           </button>
           <button>
             <i className="fa-regular fa-comment-dots"></i>
             <span></span>
-          </button>
+          </button> */}
           <div className={style.thumb} onClick={mypageClick}>
-            <img
-              src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`}
-              alt="logo"
-            />
+            <img src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`} alt="logo" />
           </div>
         </div>
 
@@ -56,25 +50,17 @@ const Header = () => {
             회원가입
           </Link>
         </div>
-        <button>
+        {/* <button>
           <i className="fa-regular fa-bell"></i>
-        </button>
+        </button> */}
         <button onClick={mypageClick}>
-          <i
-            className={isIconChanged ? "fa-solid fa-x" : "fa-solid fa-bars"}
-          ></i>
+          <i className={isIconChanged ? "fa-solid fa-x" : "fa-solid fa-bars"}></i>
         </button>
       </div>
-      <div
-        className={style.mypage}
-        style={{ display: isMypage ? "flex" : "none" }}
-      >
+      <div className={style.mypage} style={{ display: isMypage ? "flex" : "none" }}>
         <span>현재 로그인 계정</span>
         <div className={style.myprofile}>
-          <img
-            src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`}
-            alt="logo"
-          />
+          <img src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`} alt="logo" />
           <div>
             <h2>이름</h2>
             <p>abc@asdd.com</p>
@@ -91,13 +77,13 @@ const Header = () => {
         <div className={style.cate1}>
           <span>빠른메뉴</span>
           <span>메뉴</span>
-          <Link to="/">일자리 찾기</Link>
-          <Link to="/">내가 지원한 일자리</Link>
-          <Link to="/">구인글 작성</Link>
-          <Link to="/">구인글 관리</Link>
+          <Link to="/findjob">일자리 찾기</Link>
+          <Link to="/applied-list">내가 지원한 일자리</Link>
+          <Link to="/job-wirt">구인글 작성</Link>
+          <Link to="/job-offer">구인글 관리</Link>
         </div>
         <div className={style.cate2}>
-          <Link to="/">프로필 수정</Link>
+          <Link to="/mypage/ProfileEdit">프로필 수정</Link>
           <Link to="/">회원정보 수정</Link>
           <Link to="/">로그아웃</Link>
         </div>
