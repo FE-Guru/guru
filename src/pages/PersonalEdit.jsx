e658e0e7a670a16aba877f845ba76c3072215656
blunyl <aveu220@gmail.com>
2024-06-27 11:16:03 +0900

    [회원정보수정] 1. 비밀번호 제외 다른 정보 수정시에도 비밀번호 입력하게 하기 2. 다른 정보들만 수정됐을때랑 비밀번호만 수정됐을때랑 모달 내용 다르게 3. 비밀번호 변경시에 로그아웃 시키고 로그인 페이지로 이동시킴 4. 아무것도 바뀌지 않았을때도 모달 띄우기

--- src/pages/PersonalEdit.jsx
+++ src/pages/PersonalEdit.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setPageInfo } from "../store/pageInfo";
 import { url } from "../store/ref";
+import { logout } from "../store/userStore";
 import form from "../css/Form.module.css";
 import mem from "../css/Memb.module.css";
 import Modal from "../components/Modal";
@@ -13,16 +14,24 @@ const PersonalEdit = () => {
 
   const user = useSelector((state) => state.user.user);
   const emailID = user ? user.emailID : null;
-  const nickName = user ? user.nickName : null;
+  const userName = user ? user.userName : null;
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const closeAlert = () => {
     setModalAlert(null);
+  };
+
+  //비번 변경의 경우 닫기함수 따로 만듦
+  const closePwchanged = () => {
+    dispatch(logout());
+    navigate("/login");
   };
 
   const navigate = useNavigate();
   const accountDel = () => {
     navigate("/mypage/acctdelete");
   };
+
   const dispatch = useDispatch();
   const pageInfo = useMemo(
     () => ({
@@ -39,25 +48,49 @@ const PersonalEdit = () => {
   }, [dispatch, pageInfo]);
 
   const [formData, setFormData] = useState({
+    emailID: user ? user.emailID : "",
     password: "",
-    nickName: "",
-    phone: "",
-    account: "",
+    userName: user ? user.userName : "",
+    nickName: user ? user.nickName : "",
+    phone: user ? user.phone : "",
+    account: user ? user.account : "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+    if (name === "confirmPassword") {
+      setConfirmPassword(value);
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
   };
 
   const editSubmit = async (e) => {
     e.preventDefault();
 
-    if (formData.password !== formData.confirmPassword) {
+    //정보수정할때는 비번 입력 필수화시킴
+    if (!formData.password) {
+      setModalAlert("pwnotsubmit");
+      return;
+    }
+
+    //비번 일치하지 않을때
+    if (formData.password !== confirmPassword) {
       setModalAlert("pwnotmatch");
+      return;
+    }
+
+    //아무것도 변경되지 않았을때
+    if (
+      formData.nickName === user.nickName &&
+      formData.phone === user.phone &&
+      formData.account === user.account &&
+      !formData.password
+    ) {
+      setModalAlert("notchanged");
       return;
     }
 
@@ -70,8 +103,16 @@ const PersonalEdit = () => {
         body: JSON.stringify(formData),
       });
 
+      // 비번 변경된 경우 로그아웃 시킴
       if (response.ok) {
-        setModalAlert("editsuccess");
+        const data = await response.json();
+        if (data.isPwChanged) {
+          setModalAlert("pwchanged");
+        } else if (data.isUpdated) {
+          setModalAlert("editsuccess");
+        } else {
+          setModalAlert("notchanged");
+        }
       } else {
         setModalAlert("editfailed");
       }
@@ -80,7 +121,6 @@ const PersonalEdit = () => {
       setModalAlert("editfailed");
     }
   };
-
   return (
     <div className='contents'>
       <h3>{currentPage.pageName}</h3>
@@ -96,7 +136,7 @@ const PersonalEdit = () => {
             </div>
             <div className={form.formGrup}>
               <span>이름</span>
-              <span className={mem.editSpan}>{nickName}</span>
+              <span className={mem.editSpan}>{userName}</span>
             </div>
             <div className={`${form.formGrup} `}>
               <span>비밀번호</span>
@@ -115,7 +155,7 @@ const PersonalEdit = () => {
                 <input
                   type='password'
                   name='confirmPassword'
-                  value={formData.confirmPassword}
+                  value={confirmPassword}
                   onChange={handleChange}
                 />
               </div>
@@ -197,18 +237,29 @@ const PersonalEdit = () => {
               confirm={false}
             />
           )}
-          {modalAlert && (
-            <Modal show={modalAlert !== null} onClose={closeAlert} type='alert'>
-              {modalAlert === "notAuthorized" && (
-                <ModalAlert
-                  close={closeAlert}
-                  desc={"로그인이 필요한 페이지입니다."}
-                  error={true}
-                  confirm={false}
-                  goPage={"/login"}
-                />
-              )}
-            </Modal>
+          {modalAlert === "pwnotsubmit" && (
+            <ModalAlert
+              close={closeAlert}
+              desc={"정보수정을 위해 비밀번호를 입력해주세요."}
+              error={true}
+              confirm={false}
+            />
+          )}
+          {modalAlert === "pwchanged" && (
+            <ModalAlert
+              close={closePwchanged}
+              desc={"비밀번호가 변경되었습니다. 다시 로그인해주세요."}
+              error={false}
+              confirm={true}
+            />
+          )}
+          {modalAlert === "notchanged" && (
+            <ModalAlert
+              close={closeAlert}
+              desc={"변경된 정보가 없습니다."}
+              error={true}
+              confirm={false}
+            />
           )}
         </Modal>
       )}
