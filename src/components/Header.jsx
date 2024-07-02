22b5fbb5bd59e9453cbf7cd2751c0d8ed89617b5
shining-jung <tjddnjz51w@gmail.com>
2024-07-02 10:38:30 +0900

    헤더부분 html구조문, css 변경 및 스크롤이벤트 조건변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,32 +10,19 @@ import Profile from "../components/Profile";
 import style from "../css/Header.module.css";
 
 const Header = () => {
+  const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const location = useLocation();
   const [loading, setLoading] = useState(true);
-
-  const user = useSelector((state) => state.user.user);
   const [isMypage, setIsMypage] = useState(false);
   const [isIconChanged, setIsIconChanged] = useState(false);
   const [modal, setModal] = useState(null);
   const [modalAlert, setModalAlert] = useState(null);
   const [visible, setVisible] = useState(true);
-
   const emailID = user ? user?.emailID : null;
   const nickName = user ? user?.nickName : null;
   const certified = user ? user?.certified : null;
   const { isAuthenticated, isLogout } = useAuth();
-
-  const showPopup = (content) => {
-    setModal(content);
-  };
-  const closePopup = () => {
-    setModal(null);
-  };
-
-  const closeAlert = () => {
-    setModalAlert(null);
-  };
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -56,9 +43,6 @@ const Header = () => {
         if (response.ok) {
           const userInfo = await response.json();
           dispatch(userState(userInfo));
-          if (certified === false) {
-            showPopup("profile");
-          }
         } else {
           const errorData = await response.json();
           console.error("fetchProfile 에러:", errorData.message);
@@ -72,6 +56,42 @@ const Header = () => {
     };
     fetchProfile();
   }, [dispatch, location]);
+
+  useEffect(() => {
+    if (certified === false) {
+      showPopup("profile");
+    }
+  }, [certified]);
+
+  useEffect(() => {
+    setIsMypage(false);
+  }, [location]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 100) {
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+    };
+    const windowWidth = () => {
+      if (window.innerWidth >= 990) {
+        setIsMypage(false);
+        setIsIconChanged(false);
+        window.addEventListener("scroll", handleScroll);
+      } else {
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+    windowWidth();
+    window.addEventListener("resize", windowWidth);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", windowWidth);
+    };
+  }, []);
 
   const logout = async (e) => {
     e.preventDefault();
@@ -98,120 +118,46 @@ const Header = () => {
     }
   };
 
+  const closeAlert = () => {
+    setModalAlert(null);
+  };
+  const showPopup = (content) => {
+    setModal(content);
+  };
+  const closePopup = () => {
+    setModal(null);
+  };
   const mypageClick = () => {
     setIsIconChanged(!isIconChanged);
     setIsMypage(!isMypage);
   };
 
-  //mypage 다른 페이지 이동시 보이지않게
-  useEffect(() => {
-    setIsMypage(false);
-  }, [location]);
-
-  //mypage 스크롤 내리면 사라지게
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      if (scrollPosition > 100) {
-        setVisible(false);
-      } else {
-        setVisible(true);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <header className={style.header}>
       <h1>
-        <Link to='/'>
-          <img
-            src={`${process.env.PUBLIC_URL}/img/common/logo.svg`}
-            alt='logo'
-          />
+        <Link to="/">
+          <img src={`${process.env.PUBLIC_URL}/img/common/logo.svg`} alt="logo" />
         </Link>
       </h1>
       <div className={style.gnb}>
         <nav>
-          <Link to='/findjob'>일자리찾기</Link>
-          <Link to='/applied-list'>지원목록</Link>
-          <Link to='/job-offer'>구인관리</Link>
-          <Link to='/job-write'>구인글 작성</Link>
+          <Link to="/findjob">일자리찾기</Link>
+          <Link to="/applied-list">지원목록</Link>
+          <Link to="/job-offer">구인관리</Link>
+          <Link to="/job-write">구인글 작성</Link>
         </nav>
-
         {isAuthenticated ? (
           <div className={style.loginDiv}>
             <div className={style.thumb} onClick={mypageClick}>
-              {!user?.image ? (
-                <img
-                  src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`}
-                  alt='이미지 없음'
-                />
-              ) : (
-                <img src={`${url}/${user?.image}`} alt='프로필 이미지' />
-              )}
-            </div>
-            <div
-              className={style.mypage}
-              style={{ display: isMypage && visible ? "flex" : "none" }}
-            >
-              <span>현재 로그인 계정</span>
-              <div className={style.myprofile}>
-                <div className={style.profileThumb}>
-                  {!user?.image ? (
-                    <img
-                      src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`}
-                      alt='이미지 없음'
-                    />
-                  ) : (
-                    <img src={`${url}/${user?.image}`} alt='프로필 이미지' />
-                  )}
-                </div>
-                <div>
-                  <h2>{nickName ? nickName : "닉네임을 설정해주세요"}</h2>
-                  <p>{emailID}</p>
-                </div>
-              </div>
-              <div className={style.logoutBtn}>
-                <Link
-                  to='/signup'
-                  className={`${style.goJoin} btn primary yellow`}
-                >
-                  회원가입
-                </Link>
-                <Link
-                  to='/login'
-                  className={`${style.goLogin} btn primary yellow`}
-                >
-                  로그인
-                </Link>
-              </div>
-              <div className={style.cate1}>
-                <span>빠른메뉴</span>
-                <span>메뉴</span>
-                <Link to='/findjob'>일자리 찾기</Link>
-                <Link to='/applied-list'>내가 지원한 일자리</Link>
-                <Link to='/job-write'>구인글 작성</Link>
-                <Link to='/job-offer'>구인 관리</Link>
-              </div>
-              <div className={style.cate2}>
-                <Link to='/mypage/profileedit'>프로필 수정</Link>
-                <Link to='/mypage/personaledit'>회원정보 수정</Link>
-                <Link to='/logout' onClick={logout}>
-                  로그아웃
-                </Link>
-              </div>
+              {!user?.image ? <img src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`} alt="이미지 없음" /> : <img src={`${url}/${user?.image}`} alt="프로필 이미지" />}
             </div>
           </div>
         ) : (
           <div className={style.logoutDiv}>
-            <Link to='/login' className={style.goLogin}>
-              <i className='fa-regular fa-user'></i>
+            <Link to="/login" className={style.goLogin}>
+              <i className="fa-regular fa-user"></i>
             </Link>
-            <Link to='/signup' className={`${style.goJoin} btn primary yellow`}>
+            <Link to="/signup" className={`${style.goJoin} btn primary yellow`}>
               회원가입
             </Link>
           </div>
@@ -220,28 +166,70 @@ const Header = () => {
       <button className={style.ham} onClick={mypageClick}>
         <i className={isIconChanged ? "fa-solid fa-x" : "fa-solid fa-bars"}></i>
       </button>
+
+      <div className={style.mypage} style={{ display: isMypage && visible ? "flex" : "none" }}>
+        {isAuthenticated ? (
+          <>
+            <span>현재 로그인 계정</span>
+            <div className={style.myprofile}>
+              <div className={style.profileThumb}>
+                {!user?.image ? <img src={`${process.env.PUBLIC_URL}/img/common/no_img.jpg`} alt="이미지 없음" /> : <img src={`${url}/${user?.image}`} alt="프로필 이미지" />}
+              </div>
+              <div>
+                <h2>{nickName ? nickName : "닉네임을 설정해주세요"}</h2>
+                <p>{emailID}</p>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className={style.logoutBtn}>
+            <Link to="/signup" className={`${style.goJoin} btn primary yellow`}>
+              회원가입
+            </Link>
+            <Link to="/login" className={`${style.goLogin} btn primary yellow`}>
+              로그인
+            </Link>
+          </div>
+        )}
+        <ul>
+          {isAuthenticated ? <li>빠른메뉴</li> : <li>메뉴</li>}
+          <li>
+            <Link to="/findjob">일자리 찾기</Link>
+          </li>
+          <li>
+            <Link to="/applied-list">내가 지원한 일자리</Link>
+          </li>
+          <li>
+            <Link to="/job-write">구인글 작성</Link>
+          </li>
+          <li>
+            <Link to="/job-offer">구인 관리</Link>
+          </li>
+        </ul>
+        {isAuthenticated && (
+          <ul>
+            <li>
+              <Link to="/mypage/profileedit">프로필 수정</Link>
+            </li>
+            <li>
+              <Link to="/mypage/personaledit">회원정보 수정</Link>
+            </li>
+            <li>
+              <Link to="/logout" onClick={logout}>
+                로그아웃
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
       {modal && (
         <Modal show={modal !== null} onclose={closePopup} type={"profile"}>
-          {modal === "profile" && (
-            <Profile
-              show={modal !== null}
-              onclose={closePopup}
-              mode={"등록"}
-              modal={true}
-            />
-          )}
+          {modal === "profile" && <Profile show={modal !== null} onclose={closePopup} mode={"등록"} modal={true} />}
         </Modal>
       )}
       {modalAlert && (
-        <Modal show={modalAlert !== null} onClose={closeAlert} type='alert'>
-          {modalAlert === "invalidaccess" && (
-            <ModalAlert
-              close={closeAlert}
-              desc={"로그인 중 에러가 발생하였습니다."}
-              error={true}
-              confirm={false}
-            />
-          )}
+        <Modal show={modalAlert !== null} onClose={closeAlert} type="alert">
+          {modalAlert === "invalidaccess" && <ModalAlert close={closeAlert} desc={"로그인 중 에러가 발생하였습니다."} error={true} confirm={false} />}
         </Modal>
       )}
     </header>
